b7c64b5bb966b844cf5aa75a6461b7b94bdda594
Alex Day <adday@clemson.edu>
2022-10-27 18:35:34 -0400

    lint!

--- render_video.py
+++ render_video.py
@@ -1,21 +1,26 @@
-#! /usr/bin/env python 
- 
-from pathlib import Path
+#! /usr/bin/env python
+
 import argparse
-from typing import Optional 
+from pathlib import Path
+from typing import Optional
 
 import pandas as pd
+from matplotlib import animation, collections
 from matplotlib import pyplot as plt
-from matplotlib import animation
-from matplotlib import collections
-import seaborn as sns; sns.set_theme()
+
+import seaborn as sns
+
+sns.set_theme()
+
 
 class TrajectoryRenderer:
     def __init__(self, input_filename: Path, fps: float, save_filename: Optional[Path]):
         print(f"Reading in {input_filename.resolve()}...")
-        
+
         self._filename = input_filename
-        self._data = pd.read_csv(str(input_filename), names=["id", "ts", "x", "y", "gx", "gy"], skiprows=[0]).apply(pd.to_numeric)
+        self._data = pd.read_csv(
+            str(input_filename), names=["id", "ts", "x", "y", "gx", "gy"], skiprows=[0]
+        ).apply(pd.to_numeric)
         self._times = pd.unique(self._data.ts)
         self._idxs = pd.unique(self._data.id)
 
@@ -44,7 +49,9 @@ class TrajectoryRenderer:
         self._ax.set_ylim([self._min_y, self._max_y])
         self._ax.set_aspect("equal")
 
-        ani = animation.FuncAnimation(fig, self._animate, interval=1 / fps * 1000, frames=len(self._times))
+        ani = animation.FuncAnimation(
+            fig, self._animate, interval=1 / fps * 1000, frames=len(self._times)
+        )
         if save_filename:
             writer = animation.FFMpegWriter(fps=fps)
             ani.save(str(save_filename), writer=writer)
@@ -56,18 +63,20 @@ class TrajectoryRenderer:
         self._ax.set_xlim([self._min_x, self._max_x])
         self._ax.set_ylim([self._min_y, self._max_y])
 
-        self._ax.set_title(f"{self._filename.resolve()}\nTime = {str(round(self._times[time_idx], 3)).ljust(5, '0')}")
+        self._ax.set_title(
+            f"{self._filename.resolve()}\nTime = {str(round(self._times[time_idx], 3)).ljust(5, '0')}"
+        )
 
         circles = []
         for idx in self._idxs:
-            pos = self._data[(self._data.id == idx) & (self._data.ts == self._times[time_idx])][["x", "y"]].to_numpy()
+            pos = self._data[
+                (self._data.id == idx) & (self._data.ts == self._times[time_idx])
+            ][["x", "y"]].to_numpy()
 
             if len(pos) == 0:
-                continue           
+                continue
 
-            circles.append(
-                plt.Circle(pos[0], radius=0.17 if idx == -1 else 0.1)
-            )
+            circles.append(plt.Circle(pos[0], radius=0.17 if idx == -1 else 0.1))
 
         self._ax.add_collection(collections.PatchCollection(circles))
 
@@ -77,8 +86,12 @@ def main():
 
     parser.add_argument("input_file", type=Path, help="Path to the trajectory file")
 
-    parser.add_argument("--fps", type=float, default=60, help="Frames per second to render at")
-    parser.add_argument("--save", type=Path, default=None, help="Path to save the animation to")
+    parser.add_argument(
+        "--fps", type=float, default=60, help="Frames per second to render at"
+    )
+    parser.add_argument(
+        "--save", type=Path, default=None, help="Path to save the animation to"
+    )
 
     args = parser.parse_args()
 
@@ -87,6 +100,7 @@ def main():
         exit(1)
 
     TrajectoryRenderer(args.input_file, args.fps, args.save)
+
 
 if __name__ == "__main__":
     main()
