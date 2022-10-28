5649b97bf96362e38fe910626045c11882549b03
Alex Day <alexday135@gmail.com>
2022-10-27 23:14:45 -0400

    make installable

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='traj_utils',
+    version='0.0.0',
+    description="Library and tools for operating on pedestrian trajectories",
+    long_description=readme,
+    author="Alex Day",
+    author_email="alex@alexday.me",
+    url='https://github.com/AlexanderDavid/TrajectoryUtilities',
+    license=license,
+    packages=find_packages(exclude=('tests'))
+)
