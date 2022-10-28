c2a4ffd68ac998e53629aea316a5c033229a46dc
Alex Day <alexday135@gmail.com>
2022-10-27 22:14:04 -0400

    move metrics to seperate module

--- traj_utils/dataset/abstract.py
+++ traj_utils/dataset/abstract.py
@@ -26,55 +26,6 @@ class Agent:
         return f"{self.agent.idx=} w/ {len(self.positions)=}"
 
 class Dataset(ABC):
-    @staticmethod
-    def mpd(agent: Position, obstacle: Position) -> float:
-        """ Calculate the projected minimum predicted distance between two agent's at a specified
-        timestep.
-        
-
-        Args:
-            agent (Position): Ego agent
-            obstacle (Position): Other agent or obstacle
-
-        Returns:
-            float: Point-to-point minimum predicted distance 
-        """
-        ttca = Dataset.ttca(agent, obstacle)
-
-        if ttca < 0:
-            return float("inf")
-
-        p_o_a = obstacle.pos - agent.pos
-        v_o_a = obstacle.vel - agent.vel
-        dca = np.linalg.norm(p_o_a + max(0, ttca) * v_o_a)
-
-        return float(dca)
-
-    @staticmethod
-    def ttca(agent: Position, obstacle: Position) -> float:
-        """Calculate the Time to Closest Approach as defined in eqution 3 from
-        
-         Zhang, Bingqing, et al. "From HRI to CRI: Crowd Robot Interaction—
-            Understanding the Effect of Robots on Crowd Motion."
-            International Journal of Social Robotics 14.3 (2022): 631-643.
-
-        Args:
-            agent (Position): Current agent configuration
-            obstacle (Position): Current obstacle configuration
-
-        Returns:
-            float: Time till closest approach assuming two agents keep their 
-                   current heading and velocity
-        """
-        v_o_a = obstacle.vel - agent.vel
-
-        if not np.any(v_o_a):
-            return 0
-        
-        p_o_a = obstacle.pos - agent.pos
-
-        return -np.dot(p_o_a, v_o_a) / np.linalg.norm(v_o_a) ** 2
-
     @property
     @abstractmethod
     def agents(self) -> Dict[int, Agent]:
