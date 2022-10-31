5bb8dfa0235f782df5d23f6d2692f8b2f07c6c60
Alex Day <adday@clemson.edu>
2022-10-31 16:05:25 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from setuptools import setup, find_packages
 
-with open('README.md') as f:
+with open("README.md") as f:
     readme = f.read()
 
-with open('LICENSE') as f:
+with open("LICENSE") as f:
     license = f.read()
 
 setup(
     name="ardi",
-    version="0.0.0",
+    version="0.1.0",
     description="Library and tools for operating on pedestrian trajectories",
     long_description=readme,
     author="Alex Day",
     author_email="alex@alexday.me",
-    url='https://github.com/AlexanderDavid/TrajectoryUtilities',
+    url="https://github.com/AlexanderDavid/ardi",
     license=license,
-    packages=find_packages(exclude=('tests'))
+    packages=find_packages(exclude=("tests")),
 )
