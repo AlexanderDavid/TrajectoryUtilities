2021ba7feb2e01bea210457293a07fc1365b3a74
Alex Day <adday@clemson.edu>
2022-11-01 14:31:47 -0400

    remove unused import

--- ardi/prediction/linear.py
+++ ardi/prediction/linear.py
@@ -1,13 +1,15 @@
 from .abstract import Predictor, VelocityCalc
 from ..dataset import Position, Agent
 from typing import List
-from abc import abstractmethod
+
 
 class LinearPredictor(Predictor):
     def __init__(self, frames: int, velocity_calc_method: VelocityCalc):
         super().__init__(frames, velocity_calc_method)
 
-    def predict(self, ego_history: Agent, neighbors_history: List[Agent]) -> List[List[Position]]:
+    def predict(
+        self, ego_history: Agent, neighbors_history: List[Agent]
+    ) -> List[List[Position]]:
         positions = ego_history.positions
         ts = positions[1].time - positions[0].time
 
@@ -15,10 +17,9 @@ class LinearPredictor(Predictor):
 
         predictions = [
             Position(
-                positions[-1].pos + i * velocity,
-                velocity,
-                positions[-1].time + i * ts
-            ) for i in range(1, self._frames + 1)
+                positions[-1].pos + i * velocity, velocity, positions[-1].time + i * ts
+            )
+            for i in range(1, self._frames + 1)
         ]
 
         return [predictions]
