b55c2277e07f54c8a546af04271f7d5b46185299
Alex Day <adday@clemson.edu>
2023-03-29 20:37:14 -0400

    update scene viz with zucker scenarios

--- scripts/scene_viz_test.py
+++ scripts/scene_viz_test.py
@@ -1,6 +1,45 @@
 from ardi.visualization import Scene
 
-s = Scene(
-    [[0, 3], [0, 6], [0, 9], [7, -3]],
-    [[15, 3], [15, 6], [15, 9], [7, 12]])
-s.show()
+debug = False
+
+if debug:
+    # Perpendicular
+    s = Scene(
+        [[2, 0], [6, 0], [10, 0], [-4, 7]],
+        [[2, 15], [6, 15], [10, 15], [14, 7]],
+        3,
+        0.05,
+    )
+    s.show()
+
+else:
+    # Perpendicular
+    s = Scene(
+        [[2, 0], [6, 0], [10, 0], [-3, 7]],
+        [[2, 15], [6, 15], [10, 15], [14, 7]],
+        3,
+        0.05,
+    )
+    s.save("perpendicular.png", "png")
+
+    # Adjacent
+    s = Scene(
+        [[2, 0], [6, 0], [10, 0], [-2, 3]],
+        [[2, 15], [6, 15], [10, 15], [15, 12]],
+        3,
+        0.05,
+    )
+    s.save("adjacent.png", "png")
+
+    # Opposite
+    s = Scene(
+        [[2, 0], [6, 0], [10, 0], [-3, 7]],
+        [[2, 15], [6, 15], [10, 15], [15, 0]],
+        3,
+        0.05,
+    )
+    s.save("opposite.png", "png")
+
+    # Intersection
+    s = Scene([[0, 0], [3, 15], [-5, 7.5]], [[0, 15], [3, 0], [7, 7.5]], 2, 0.01)
+    s.save("intersection.png", "png")
